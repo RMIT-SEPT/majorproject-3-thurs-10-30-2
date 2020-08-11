37e48392feaac5058a7eedca2e10a5c5ff34c5cc
Chris Hodgen <s3031209@student.rmit.edu.au>
2020-08-11 17:56:54 +1000

    Implemented basic user deletion.

--- BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/web/UserController.java
+++ BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/web/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -31,5 +28,12 @@ public class UserController {
 
         User user1 = userService.saveOrUpdateUser(user); //tmp user
         return new ResponseEntity<User>(user, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        // TODO: Implement security checks before proceeding with deletion.
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
