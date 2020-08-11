c1fe460ab9ed0bb93f70cb476884cd0baf6c81ab
Chris Hodgen <s3031209@student.rmit.edu.au>
2020-08-11 17:16:11 +1000

    Updated UserController to return meaningful collection of errors.

--- BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/web/UserController.java
+++ BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/web/UserController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/User")
@@ -23,9 +25,10 @@ public class UserController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewUser(@Valid @RequestBody User user, BindingResult result){
-    if(result.hasErrors()){
-        return new ResponseEntity<String>("Invalid User Object", HttpStatus.BAD_REQUEST);
-    }
+        if(result.hasErrors()) {
+            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+        }
+
         User user1 = userService.saveOrUpdateUser(user); //tmp user
         return new ResponseEntity<User>(user, HttpStatus.CREATED);
     }
