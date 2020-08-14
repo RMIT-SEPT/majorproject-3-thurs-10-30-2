f1f6ae8798831c10249dc448569b8fc74635cc0b
Chris Hodgen <s3031209@student.rmit.edu.au>
2020-08-14 21:46:15 +1000

    Added additional test for deleting user.

--- BackEnd/project/src/test/java/com/rmit/sept/majorproject/project/services/UserServiceTest.java
+++ BackEnd/project/src/test/java/com/rmit/sept/majorproject/project/services/UserServiceTest.java
@@ -1,11 +1,13 @@
 package com.rmit.sept.majorproject.project.services;
 
 import com.rmit.sept.majorproject.project.model.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 
 @SpringBootTest
@@ -27,5 +29,14 @@ public class UserServiceTest {
     void delete_DoesNotThrow_IfUserExists() {
         User newUser = userService.saveOrUpdateUser(user);
         userService.delete(newUser.getId());
+    }
+
+    @Test
+    void delete_ThrowsException_IfUserDoesNotExist(){
+        User newUser = userService.saveOrUpdateUser(user);
+        userService.delete(newUser.getId());    // This delete should succeed
+
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> userService.delete(newUser.getId()),
+                "Deleting a user which doesn't exist should throw org.springframework.dao.EmptyResultDataAccessException.");
     }
 }
