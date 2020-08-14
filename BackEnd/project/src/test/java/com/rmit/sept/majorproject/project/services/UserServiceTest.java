e277d41081e1e01827337dfb332bfcc8c147e597
Chris Hodgen <s3031209@student.rmit.edu.au>
2020-08-14 21:44:46 +1000

    Added a couple of tests for deleting a user.

--- BackEnd/project/src/test/java/com/rmit/sept/majorproject/project/services/UserServiceTest.java
+++ BackEnd/project/src/test/java/com/rmit/sept/majorproject/project/services/UserServiceTest.java
@@ -1,13 +1,31 @@
 package com.rmit.sept.majorproject.project.services;
 
+import com.rmit.sept.majorproject.project.model.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
 
-    @Test
-    void delete_IsCorrect_IfUserExists() {
+    @Autowired
+    private UserService userService;
+    private User user;
 
+    @BeforeAll
+    public void setup() {
+        user = new User();
+        user.setName("Test User");
+        user.setEmail("test@email.com");
+    }
+
+    @Test
+    void delete_DoesNotThrow_IfUserExists() {
+        User newUser = userService.saveOrUpdateUser(user);
+        userService.delete(newUser.getId());
     }
 }
