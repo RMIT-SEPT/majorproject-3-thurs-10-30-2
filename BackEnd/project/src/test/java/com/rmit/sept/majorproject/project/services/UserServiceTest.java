0d6bc39075566371458d2121d9fdd9fa2e836ca0
Chris Hodgen <s3031209@student.rmit.edu.au>
2020-08-14 20:04:50 +1000

    Setting up the test class for the UserService class

---
+++ BackEnd/project/src/test/java/com/rmit/sept/majorproject/project/services/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.majorproject.project.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Test
+    void delete_IsCorrect_IfUserExists() {
+
+    }
+}
