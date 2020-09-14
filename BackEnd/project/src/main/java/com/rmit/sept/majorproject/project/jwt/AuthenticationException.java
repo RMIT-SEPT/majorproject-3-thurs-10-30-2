2b0bc6793d90dbbd179b4649bffab7549ce07fc0
lcave <lucacave2@gmail.com>
2020-09-14 19:26:26 +1000

    Added JWT auth

---
+++ BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/jwt/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.rmit.sept.majorproject.project.authentication.springbootjwtauthloginlogout.jwt;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
