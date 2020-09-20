e7ab9ac2480cb55c5ca763943800ffb523ea6628
ryan <s3718888@student.rmit.edu.au>
2020-09-20 20:18:50 +1000

    UserService.java Added Email Regex and password length checks

--- BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/services/UserService.java
+++ BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/services/UserService.java
@@ -5,14 +5,22 @@ import com.rmit.sept.majorproject.project.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     public User saveOrUpdateUser(User user){
-        //TODO: implement save and update logic
-        if(user.getName().length() > 2) {
+
+        // Email Regex
+        String regex = "(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(user.getEmail());
+
+        if(user.getName().length() > 2 && user.getPassword().length() >= 8 && matcher.matches()) {
             return userRepository.save(user);
         }
         return null;
