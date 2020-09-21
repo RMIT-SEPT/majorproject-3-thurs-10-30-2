7c72df56e1dda9387afefef21f058f1edce2739f
lcave <lucacave2@gmail.com>
2020-09-21 11:51:53 +1000

    Added redirects to login page buttons

--- FrontEnd/agme-booking/src/Components/Homepage/SignUp.js
+++ FrontEnd/agme-booking/src/Components/Homepage/SignUp.js
@@ -14,24 +14,24 @@ class SignUp extends React.Component {
                 <Form>
                     <Form.Group controlId="formName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter full name" />
+                        <Form.Control type="text" placeholder="Enter full name" value="John Doe"/>
                     </Form.Group>
                     <Form.Group controlId="formSignupEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" value="john.doe@gmail.com"/>
                     </Form.Group>
                     <Form.Group controlId="formSignupPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" value="abc123"/>
                     </Form.Group>
                     <Form.Group controlId="formSignupPasswordConfirmation">
                         <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" value="abc123"/>
                     </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                </Button>
                 </Form>
+                <Button variant="primary" type="submit" onClick={() => window.location.href = '/dashboard'}>
+                    Submit
+                </Button>
                 <Button variant="light" onClick={() => this.props.setForm(0)}>Back</Button>{' '}
             </div>
         );
