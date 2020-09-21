e3db27c12717a8ec41f44ee5cb70c9cdcf937ac2
Richard Gao <s3784324@student.rmit.edu.au>
2020-09-21 21:47:56 +1000

    completed backend TF files for deployment

--- BackEnd/project/terraform/app.tf
+++ BackEnd/project/terraform/app.tf
@@ -56,3 +56,25 @@ resource "aws_autoscaling_group" "bar" {
   vpc_zone_identifier  = [aws_subnet.private_az1.id, aws_subnet.private_az2.id, aws_subnet.private_az3.id]
   target_group_arns    = [aws_lb_target_group.todo_app.arn]
 }
+resource "aws_lb" "todo_app" {
+  name               = "todo-app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_http_ssh.id]
+  subnets            = [aws_subnet.private_az1.id, aws_subnet.private_az2.id, aws_subnet.private_az3.id]
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.todo_app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.todo_app.arn
+  }
+}
