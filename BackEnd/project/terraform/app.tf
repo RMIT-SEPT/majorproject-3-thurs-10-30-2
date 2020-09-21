23897d2ae21ba7ed605b4169011619fbc8be91ad
Richard Gao <s3784324@student.rmit.edu.au>
2020-09-21 21:27:44 +1000

    updated terraform files
    launch config added
    auto scaling group added
    target group for load balancer created

--- BackEnd/project/terraform/app.tf
+++ BackEnd/project/terraform/app.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "allow_http_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-ingress {
+  ingress {
     description = "http from internet"
     from_port   = 80
     to_port     = 80
@@ -30,4 +30,29 @@ ingress {
   tags = {
     Name = "allow_http_ssh"
   }
+}
+
+resource "aws_launch_configuration" "todo_app" {
+  name            = "web_config"
+  image_id        = var.ami_id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.allow_http_ssh.id]
+
+  key_name = aws_key_pair.deployer.key_name
+}
+
+resource "aws_lb_target_group" "todo_app" {
+  name     = "todo-app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_autoscaling_group" "bar" {
+  name                 = "terraform-autosacle-AGME"
+  max_size             = 1
+  min_size             = 1
+  launch_configuration = aws_launch_configuration.todo_app.name
+  vpc_zone_identifier  = [aws_subnet.private_az1.id, aws_subnet.private_az2.id, aws_subnet.private_az3.id]
+  target_group_arns    = [aws_lb_target_group.todo_app.arn]
 }
--- BackEnd/project/terraform/terraform.tfvars
+++ BackEnd/project/terraform/terraform.tfvars
@@ -1,1 +1,2 @@
-public_key = "insert ssh -rsa key"
+public_key = "ssh-rsa key"
+ami_id     = "ami-055d18a0205f9ddb1"
