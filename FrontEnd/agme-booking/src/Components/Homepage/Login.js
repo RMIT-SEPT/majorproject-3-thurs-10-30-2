a1d0c8c99cdafb5e3222ba5ad522878458d0149e
lcave <lucacave2@gmail.com>
2020-10-07 14:41:52 +1100

    Added axios login request

--- FrontEnd/agme-booking/src/Components/Homepage/Login.js
+++ FrontEnd/agme-booking/src/Components/Homepage/Login.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-
+import axios from 'axios';
+import jwt_decode from "jwt-decode";
 
 class Login extends React.Component {
 
     render() {
 
         function login() {
-            window.location.href = '/admin'
-            //TODO implement login code
+            axios({
+                method: "POST",
+                url: 'http://localhost:3000/api/users/login',
+                headers: {},
+                data: {
+                    "username": document.getElementById("formLoginEmail").value,
+                    "password": document.getElementById("formLoginEmail").value
+                }
+            }).then(function (response) {
+                if (response.status === 201) {
+                    var data = jwt_decode(response.token.split(" ")[1])
+                    console.log(data)
+                }
+                console.log(response);
+            });
         }
+
 
         return (
             <div className="overlay-form">
@@ -20,12 +35,12 @@ class Login extends React.Component {
                 <Form>
                     <Form.Group controlId="formLoginEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" value="admin@agme.com.au" />
+                        <Form.Control type="email" placeholder="Enter email"/>
                     </Form.Group>
 
                     <Form.Group controlId="formLoginPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" value="abc123" />
+                        <Form.Control type="password" placeholder="Password"/>
                     </Form.Group>
                     <Form.Group controlId="formRememberCheckbox">
                         <Form.Check type="checkbox" label="Remember me" />
