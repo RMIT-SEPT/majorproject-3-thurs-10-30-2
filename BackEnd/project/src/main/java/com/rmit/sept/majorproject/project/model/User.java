cfb59103e819aed28052c93134eb19c1302086f4
lcave <lucacave2@gmail.com>
2020-10-08 14:33:47 +1100

    Fixed infinite recursion in user -> business relationship

--- BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/model/User.java
+++ BackEnd/project/src/main/java/com/rmit/sept/majorproject/project/model/User.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.Set;
 
 @Entity
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "bookingsAsCustomer", "bookingsAsWorker", "business"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "bookingsAsCustomer", "bookingsAsWorker"})
 public class User implements UserDetails {
 
     public enum AccountType {
