3a87b20207960d5ccf7e1497e00eb232c459e687
lcave <lucacave2@gmail.com>
2020-10-08 11:23:59 +1100

    Fix error in private routes

--- FrontEnd/agme-booking/src/Components/PrivateRoute.jsx
+++ FrontEnd/agme-booking/src/Components/PrivateRoute.jsx
@@ -4,13 +4,13 @@ import { Route, Redirect } from 'react-router-dom';
 // from: https://jasonwatmore.com/post/2019/04/06/react-jwt-authentication-tutorial-example
 export const PrivateRoute = ({ component: Component, userType: userType, ...rest }) => (
     <Route {...rest} render={props => {
-        const currentUser = localStorage.user;
-        if (!currentUser || currentUser.account_type !== userType) {
+        const currentUser = JSON.parse(localStorage.user);
+        if (!currentUser || currentUser.accountType !== userType) {
             // not logged in so redirect to login page with the return url
             return <Redirect to={{ pathname: '/' }} />
         }
-        if (currentUser.account_type === userType) {
-            return <Component {...props} />
+        if (currentUser.accountType === userType) {
+            return Component
         }
     }} />
 )
