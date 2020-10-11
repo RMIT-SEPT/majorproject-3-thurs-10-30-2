d0a1e689b3b5110c222a4b009b3cb8a23948b0bc
lcave <lucacave2@gmail.com>
2020-10-11 20:07:53 +1100

    Readded vansihed code

--- FrontEnd/agme-booking/src/Components/PrivateRoute.jsx
+++ FrontEnd/agme-booking/src/Components/PrivateRoute.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-// from: https://jasonwatmore.com/post/2019/04/06/react-jwt-authentication-tutorial-example
-export const PrivateRoute = ({ component: Component, userType: userType, ...rest }) => (
+// from: https://jasonwatmore.com/post/2019/04/06/react-jwt-authentication-tutorial-example with a little customisation
+export const PrivateRoute = ({ component: Component, userType, ...rest }) => (
     <Route {...rest} render={props => {
-        const currentUser = localStorage.user;
-        if (!currentUser || currentUser.account_type !== userType) {
+        const currentUser = JSON.parse(localStorage.user);
+        if (!currentUser || currentUser.accountType !== userType) {
             // not logged in so redirect to login page with the return url
             return <Redirect to={{ pathname: '/' }} />
         }
-        if (currentUser.account_type === userType) {
-            return <Component {...props} />
+        if (currentUser.accountType === userType) {
+            return Component
         }
     }} />
 )
